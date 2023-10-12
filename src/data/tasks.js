d153639914080e6039f0850349b896fdf0b1e5ee
alejastonem <88559310+alejastonem@users.noreply.github.com>
2023-10-12 14:42:54 -0600

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Esta es mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Esta es mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Esta es mi tercer tarea",
+  },
+];
